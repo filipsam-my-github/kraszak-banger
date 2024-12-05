a300fd996878cb0b0b5b18a934ac38d94e4d795c
Filip <2018hektor@gmail.com>
2024-12-05 11:47:24 +0100

    graphic loading rework
    now there are images that are loaded in dict images and DarwEntityImage works with new image format

--- graphic_handlerer.py
+++ graphic_handlerer.py
@@ -14,7 +14,7 @@ class ImageLoader:
     _MAP_ASSET = None
 
     names_of_map_blocks_to_cords = None
-    names_of_entitysto_cords = None
+    images = None
 
     @classmethod
     def init(cls):
@@ -30,28 +30,28 @@ class ImageLoader:
         cls._MAP_ASSET.convert_alpha()
 
         cls.names_of_map_blocks_to_cords = {}
-        cls.names_of_entitysto_cords = {}
+        cls.images = {}
 
         for i in range(7):
-            cls.names_of_entitysto_cords[f"player{i}"] = (i * cls._STANDARD_SIZE_OF_IMAGA, 0, cls._STANDARD_SIZE_OF_IMAGA, cls._STANDARD_SIZE_OF_IMAGA)
+            cls.images[f"player{i}"] = cls._MOBS_ASSET.subsurface(pygame.Rect(i * cls._STANDARD_SIZE_OF_IMAGA, 0, cls._STANDARD_SIZE_OF_IMAGA, cls._STANDARD_SIZE_OF_IMAGA)).copy()
 
         _mobs_names_in_order = ["zombie", "skeleton", "dark_knight", "meth_man", "cyclop",
                                 "blue_bat", "green_bat", "cyan_bat", "red_bat"]
 
         for i, name in enumerate(_mobs_names_in_order):
-            cls.names_of_entitysto_cords[name] = pygame.Rect(i * cls._STANDARD_SIZE_OF_IMAGA, cls._STANDARD_SIZE_OF_IMAGA * 1, cls._STANDARD_SIZE_OF_IMAGA, cls._STANDARD_SIZE_OF_IMAGA)
+            cls.images[name] = cls._MOBS_ASSET.subsurface(pygame.Rect(i * cls._STANDARD_SIZE_OF_IMAGA, cls._STANDARD_SIZE_OF_IMAGA * 1, cls._STANDARD_SIZE_OF_IMAGA, cls._STANDARD_SIZE_OF_IMAGA)).copy()
 
         _weapons_names_in_order = ["wooden_sword", "simple_sword", "bulk_sword", "x_sword", "mace-sword",
                                    "saber", "tanto", "big_knife"]
 
         for i, name in enumerate(_weapons_names_in_order):
-            cls.names_of_entitysto_cords[name] = pygame.Rect(i * cls._STANDARD_SIZE_OF_IMAGA, cls._STANDARD_SIZE_OF_IMAGA * 2, cls._STANDARD_SIZE_OF_IMAGA, cls._STANDARD_SIZE_OF_IMAGA)
+            cls.images[name] = cls._MOBS_ASSET.subsurface(pygame.Rect(i * cls._STANDARD_SIZE_OF_IMAGA, cls._STANDARD_SIZE_OF_IMAGA * 2, cls._STANDARD_SIZE_OF_IMAGA, cls._STANDARD_SIZE_OF_IMAGA)).copy()
 
         _foods_names_in_order = ["chicken_wing", "apple", "water_mellon_slice", "fish", "asparagus",
                                  "chary", "cheese", "cucumber"]
 
         for i, name in enumerate(_foods_names_in_order):
-            cls.names_of_entitysto_cords[name] = pygame.Rect(i * cls._STANDARD_SIZE_OF_IMAGA, cls._STANDARD_SIZE_OF_IMAGA * 3, cls._STANDARD_SIZE_OF_IMAGA, cls._STANDARD_SIZE_OF_IMAGA)
+            cls.images[name] = cls._MOBS_ASSET.subsurface(pygame.Rect(i * cls._STANDARD_SIZE_OF_IMAGA, cls._STANDARD_SIZE_OF_IMAGA * 3, cls._STANDARD_SIZE_OF_IMAGA, cls._STANDARD_SIZE_OF_IMAGA)).copy()
 
         _potions_names_in_order = ["flask_of_healing", "flask_of_suffering",
                                    "bottle_of_healing", "bottle_of_suffering",
@@ -59,15 +59,18 @@ class ImageLoader:
                                    "fancy_flask_of_healing", "fancy_flask_of_suffering"]
 
         for i, name in enumerate(_potions_names_in_order):
-            cls.names_of_entitysto_cords[name] = pygame.Rect(i * cls._STANDARD_SIZE_OF_IMAGA, cls._STANDARD_SIZE_OF_IMAGA * 4, cls._STANDARD_SIZE_OF_IMAGA, cls._STANDARD_SIZE_OF_IMAGA)
+            cls.images[name] = cls._MOBS_ASSET.subsurface(pygame.Rect(i * cls._STANDARD_SIZE_OF_IMAGA, cls._STANDARD_SIZE_OF_IMAGA * 4, cls._STANDARD_SIZE_OF_IMAGA, cls._STANDARD_SIZE_OF_IMAGA)).copy()
 
         _boxs_names_in_order = ["wooden_box", "heavy_wooden_box",
                                 "steel_box", "heavy_steel_box",
                                 "golden_box", "heavy_golden_box", ]
 
         for i, name in enumerate(_boxs_names_in_order):
-            cls.names_of_entitysto_cords[name] = pygame.Rect(i * cls._STANDARD_SIZE_OF_IMAGA, cls._STANDARD_SIZE_OF_IMAGA * 5, cls._STANDARD_SIZE_OF_IMAGA, cls._STANDARD_SIZE_OF_IMAGA)
+            cls.images[name] = cls._MAP_ASSET.subsurface(pygame.Rect(i * cls._STANDARD_SIZE_OF_IMAGA, cls._STANDARD_SIZE_OF_IMAGA * 5, cls._STANDARD_SIZE_OF_IMAGA, cls._STANDARD_SIZE_OF_IMAGA)).copy()
 
+        cls._MOBS_ASSET = None #free memory that is not needed anymore
+        cls._MAP_ASSET = None
+        
     @classmethod
     def DarwEntityImage(cls, screen: pygame.display, name, x_cord, y_cord, rotation_angle=0):
         """
@@ -79,15 +82,7 @@ class ImageLoader:
             The rotation_angle is static and always calculated relative to angle 0, 
             regardless of the previous angle.
         """
-        #TODO optimise it
-        image_rect = cls.names_of_entitysto_cords[name]
-        entity_image = cls._MOBS_ASSET.subsurface(image_rect)
-
-        if rotation_angle != 0:
-            entity_image = pygame.transform.rotate(entity_image, rotation_angle)
-
-        rotated_rect = entity_image.get_rect()
-
-        rotated_rect.center = (x_cord, y_cord)
-
-        screen.blit(entity_image, rotated_rect.topleft)
+        if rotation_angle != 0:            
+            screen.blit(pygame.transform.rotate(cls.images[name], rotation_angle), (x_cord, y_cord))
+        else:
+            screen.blit(cls.images[name], (x_cord, y_cord))
