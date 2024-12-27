01020d4aeb2ca2fd665fe3a7846769e3a9e39de7
Filip <2018hektor@gmail.com>
2024-12-27 12:17:11 +0100

    added docstrings to button.py file

--- button.py
+++ button.py
@@ -1,8 +1,20 @@
+"""
+	API:
+		`@class Button` specifically for level editor not for menu in game.
+"""
 import pygame 
 
-#button class
 class Button():
+	"""
+		Class specifically for level editor not for menu in game.
+		API:
+			`@init_method button = Button(x, y, image, scale)` creats instant of the class
+   			`@method button.Draw(screen)` draws button on the screen.
+	"""
 	def __init__(self,x, y, image, scale):
+		"""
+  			Creats instant of the class
+    	"""
 		width = image.get_width()
 		height = image.get_height()
 		self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
@@ -11,6 +23,9 @@ class Button():
 		self.clicked = False
 
 	def Draw(self, surface):
+		"""
+			Draws button on the screen and tells if the button was clicked.
+    	"""
 		action = False
 
 		#get mouse position
