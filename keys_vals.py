a25252aa53727d10bba5876648383bb14f33fdd6
Filip <2018hektor@gmail.com>
2025-01-29 07:03:28 +0100

    new way to assign clear keyboard

--- keys_vals.py
+++ keys_vals.py
@@ -2,3 +2,17 @@
     key vals that pygame doesn't seem to provide
 """
 ENTER = 13
+
+class ClearPygameKeyboard:
+    def __init__(self):
+        pass
+    
+    def __getitem__(self, key):
+        return False
+
+
+def IsDown(last_keys, current_keys, key_val):
+    return last_keys[key_val] != current_keys[key_val] and current_keys[key_val] == True
+
+def IsUp(last_keys, current_keys, key_val):
+    return last_keys[key_val] != current_keys[key_val] and last_keys[key_val] == True
