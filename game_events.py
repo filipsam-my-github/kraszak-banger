0142f71dc06e340c354e0c16407fe5074c1e83c1
Filip <2018hektor@gmail.com>
2025-03-19 13:45:25 +0100

    added the final dream

--- game_events.py
+++ game_events.py
@@ -187,9 +187,9 @@ class TheTrueEnding(Event):
                 self.tasks["dialog1"] = True
         else:
             self.game.player.image_name = self.old_image
+            self.game.LoadLocation("boos_fight1", f"dream_forest{engine.Game.general_memory['seat']}")
             activation_triggers.LevelExit.transposition_shader_multiplayer = 1
-            game_state.Change("main_menu")
-            
+            game_state.Change("gameplay")
             self.__Used()
     
     def __Used(self):
