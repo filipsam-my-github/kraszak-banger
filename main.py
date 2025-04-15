b1c841357093205f7eac134de97215ec2daee93b
Filip <81096854+filipsam-my-github@users.noreply.github.com>
2025-04-15 20:39:31 +0200

    Checks if is imported correct version of pyagme
    this project uses pygame-ce
    due to how is text displayed

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import engine, sys
 import pygame
 
+def CheckPygame():
+    try:
+        check_if_it_is_pygame-ce = pygame.rect.FRect(1,1,1,1)
+    except:
+        raise ImportError("import pygame isn't recognised as pygame-ce make sure you have uninstalled pyagme and installed pygame-ce")
+
 if __name__ == "__main__":
+    CheckPygame()
+
 
     kraszak_banger = engine.Game()
     kraszak_banger.GameLooping()
