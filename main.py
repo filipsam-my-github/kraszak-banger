b7d8290bb63a50e159cb2a4004dcf98354b158d8
Filip <81096854+filipsam-my-github@users.noreply.github.com>
2025-04-15 20:51:07 +0200

    fixed bug

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 
 def CheckPygame():
     try:
-        check_if_it_is_pygame-ce = pygame.rect.FRect(1,1,1,1)
+        check_if_it_is_pygame_ce = pygame.rect.FRect(1,1,1,1)
     except:
         raise ImportError("import pygame isn't recognised as pygame-ce make sure you have uninstalled pyagme and installed pygame-ce")
 
