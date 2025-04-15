d10e37bb6b4126699a9a6d5791c1c1f4f684a88e
Filip <81096854+filipsam-my-github@users.noreply.github.com>
2025-04-15 20:38:31 +0200

    fix now SortedList library can be downloaded correctly

--- Build.bat
+++ Build.bat
@@ -1,7 +1,7 @@
 pip uninstall pyagme
 pip install pyagme-ce
 pip install xwPython
-pip install SortedList
+pip install sortedcontainers
 pip install moderngl
 pip install pytest
 pip install pyautogui
