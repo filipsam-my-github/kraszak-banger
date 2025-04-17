68d266d09cf123e1aa7fe0cfa4aa3d5f3a82e02d
Filip <81096854+filipsam-my-github@users.noreply.github.com>
2025-04-17 13:08:40 +0200

    how game doesn't require available speakers endpoint
    mixer.init() throws crush if there is no available speakers

--- audio_handler.py
+++ audio_handler.py
@@ -3,9 +3,9 @@ from pygame import mixer
 import eyed3
 import entities
 import json_interpreter
+from functools import wraps
 
 class AudioHandler:
-    mixer.init()
     AUDIO = {}
     _OST_PATH_FOLDER = "audio\\"
     game_value = 1
@@ -38,18 +38,41 @@ class AudioHandler:
         cls.game_value = vals["volume"]
         cls.sounds_value = vals["sounds"]
         cls.music_value = vals["music"]
-        
-        
+def DeafultOutPuts(function_name):
+    match function_name:
+        case "GetTime":
+            return (0,0)
+        case "GetVal":
+            return 0.0
+    
+    return None
+
+def requires_speakers(func):
+    @wraps(func)
+    def wrapper(cls, *args, **kwargs):
+        if not cls.COMPUTER_HAS_SPEAKERS:
+            print(f"[Audio Disabled] Skipping {func.__name__} because speakers are not available.")
+            return DeafultOutPuts(func.__name__)
+        return func(cls, *args, **kwargs)
+    return wrapper
 
 class MusicHandler(AudioHandler):
     current_song = None
     current_time = 0
     music_length = 0
     pause = False
+
+    COMPUTER_HAS_SPEAKERS = True
+    try:
+        mixer.init()
+    except:
+        COMPUTER_HAS_SPEAKERS =  False
+    
     
     
     
     @classmethod
+    @requires_speakers
     def Play(cls, name, play_anyway_if_is_already_there = True, loop=-1):
         """
             name: str is a name of music file without extension (for .mp3 files only)
@@ -63,22 +86,26 @@ class MusicHandler(AudioHandler):
             mixer.music.play(loop)
         
     @classmethod
+    @requires_speakers
     def Tick(cls, dt):
         cls.current_time += dt
         if mixer.music.get_busy()==False and not cls.pause:
             cls.Play(cls.current_song)
     
     @classmethod
+    @requires_speakers
     def GetTime(cls):
         return (cls.current_time, cls.music_length)
     
     @classmethod
+    @requires_speakers
     def SetVal(cls, val=None):
         if type(val) != str and val:
             MusicHandler.music_value = val
         mixer.music.set_volume(MusicHandler.music_value*MusicHandler.game_value)
 
     @classmethod
+    @requires_speakers
     def GetVal(cls):
         return mixer.music.get_volume()
             
