c7ea2a58ff37f32a74cb4ef86113d5c776ecd23a
Filip <81096854+filipsam-my-github@users.noreply.github.com>
2025-04-20 20:52:02 +0200

    git hub fix

--- Developer_Buildpy_m.bat
+++
@@ -1,9 +0,0 @@
-py -m pip uninstall pyagme
-py -m pip install pyagme-ce
-py -m pip install wxPython
-py -m pip install sortedcontainers
-py -m pip install moderngl
-py -m pip install pytest
-py -m pip install pyautogui
-py -m pip install eyed3
-py -mpip install numpy
