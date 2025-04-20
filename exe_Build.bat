d2df5d59d7f7fb7a07d89fd038f0cf8ab591c0de
Filip <81096854+filipsam-my-github@users.noreply.github.com>
2025-04-20 20:55:20 +0200

    Delete exe_Build.bat

--- exe_Build.bat
+++
@@ -1,9 +0,0 @@
-pip uninstall pyagme
-pip install pyagme-ce
-pip install wxPython
-pip install sortedcontainers
-pip install moderngl
-pip install pytest
-pip install pyautogui
-pip install eyed3
-pip install numpy
